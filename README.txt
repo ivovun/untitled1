d4865f8e90dd3eedc982abfa079722c950275e61
Vladimir Filippov <sergeiych@gmail.com>
2020-01-04 04:43:26 +0300

    first commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+на основе тутариала
+
+https://www.boraji.com/spring-hello-world-example-with-annotation
---
+++ src/main/java/AppConfig.java
@@ -0,0 +1,10 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean(name="myBean")
+    public HelloWorld getHelloWorld(){
+        return new HelloWorld();
+    }
+}
